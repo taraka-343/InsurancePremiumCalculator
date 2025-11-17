2ffbeabfd7bcebd6cd013257b3d0be8facf257cc
taraka-343 <usivakumar343@gmail.com>
2025-11-17 19:50:03 +0530

    Adding dictionary for factor for each occupation

--- Services/PremiumService.cs
+++ Services/PremiumService.cs
@@ -2,5 +2,12 @@
 {
     public class PremiumService
     {
+        Dictionary<string, decimal> _ratingFactors = new()
+        {
+            { "Professional", 1.5m },
+            { "White Collar", 2.25m },
+            { "Light Manual", 11.50m },
+            { "Heavy Manual", 31.75m }
+        };
     }
 }
