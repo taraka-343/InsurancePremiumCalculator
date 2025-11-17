6414c1e43a3a83e0222c50eb6697c3b4f540207e
taraka-343 <usivakumar343@gmail.com>
2025-11-17 19:34:11 +0530

    adding models folder and PremiumRequest model class

---
+++ Models/PremiumRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace InsurancePremiumCalcBE.Models
+{
+    public class PremiumRequest
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Occupation { get; set; }
+        public decimal DeathSumInsured { get; set; }
+    }
+}
